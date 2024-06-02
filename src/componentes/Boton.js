07b4090ab60c085be49ab84ce5832f988ee08742
Juan Pablo <juanpablocaceres8@gmail.com>
2024-06-02 10:12:28 -0300

    Retoques finales

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -5,15 +5,26 @@ function Boton(props){
 
     const esOperador = valor => {
         return isNaN(valor) && (valor != '.') && (valor != '=');
-    }
+    };
 
-    return(
-        <div
-        className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}
-        onClick = {() => props.manejarClick(props.children)}>
-            {props.children}
-        </div>
-    );
+    if (esOperador(props.children)){
+        return(
+            <div
+            className='boton-contenedor operador'
+            onClick = {() => props.manejarClick(props.children)}>
+                {props.children}
+            </div>
+        );
+    }
+    else{
+        return(
+            <div
+            className='boton-contenedor'
+            onClick = {() => props.manejarClick(props.children)}>
+                {props.children}
+            </div>
+        );
+    }
 }
 
 export default Boton;
